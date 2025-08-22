3bb25016515973c6b150e38d9e27ff1ceb400d2c
Rahul Bhavsar <rahulbhavsar1210@gmail.com>
2025-08-22 19:50:21 +0530

    my changes

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-const {app } = require("./api/index"); // Your Express app
-const PORT = process.env.PORT || 5000;
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./ConfigData/db");
+const authRoutes = require('./routes/authroutes')
+const userRoutes = require('./routes/userroutes')
+const studentRoutes = require("./routes/studentRoutes");
+const classRoutes = require('./routes/classroutes')
+const paymentRoutes = require("./routes/paymentRoutes");
 
+const exptraRoutes= require('./routes/extraroutes')
+dotenv.config(); // make sure to load .env first
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/items", require("./routes/itemroutes"));
+app.use("/auth", authRoutes);
+app.use("/users",userRoutes);
+app.use("/students",studentRoutes);
+app.use("/class",classRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/extra",exptraRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
