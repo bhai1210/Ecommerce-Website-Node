ebd7f1387422ecff930597055da16d127a26e84e
Rahul Bhavsar <rahulbhavsar1210@gmail.com>
2025-08-22 16:58:50 +0530

    ddd

--- routes/studentinforoutes.js
+++ routes/studentinforoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const studentController = require("../controllers/studentInfoController");
+const crypto = require("crypto");
 
 const router = express.Router();
 
@@ -11,11 +12,24 @@ const storage = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    const uniqueSuffix = crypto.randomBytes(8).toString("hex");
+    cb(null, uniqueSuffix + path.extname(file.originalname));
   }
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png|gif/;
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = fileTypes.test(file.mimetype);
+    if (extname && mimetype) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only images are allowed!"));
+    }
+  }
+});
 
 // Routes
 router.get("/", studentController.getAllStudents);
