8942dfe26e3c0d7836ebe3efde674c024010a900
Rahul Bhavsar <rahulbhavsar1210@gmail.com>
2025-08-22 17:18:37 +0530

    Reapply "ddd"
    This reverts commit d88c84b2d4c6a8fc6635321329b816a49cff543a.

--- routes/extraroutes.js
+++ routes/extraroutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const extraController= require('../Controllers/extracontroller')
-const { route } = require('./authroutes')
+const extraController = require('../Controllers/extracontroller')
 
-router.get('/',extraController.getalluser)
-router.get('/:id',extraController.getuserbyid)
-router.post('/',extraController.createuser)
-router.put('/:id',extraController.updateuser)
-router.delete('/:id',extraController.deleteuser)
+// CRUD routes
+router.get('/', extraController.getalluser)
+router.get('/:id', extraController.getuserbyid)
+router.post('/', extraController.createuser)
+router.put('/:id', extraController.updateuser)
+router.delete('/:id', extraController.deleteuser)
 
 module.exports = router
