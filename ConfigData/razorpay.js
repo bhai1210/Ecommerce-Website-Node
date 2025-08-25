c2f1a8d6d915702ded5615c968bd85178597c161
Rahul Bhavsar <rahulbhavsar1210@gmail.com>
2025-08-25 17:38:36 +0530

    dfd

--- ConfigData/razorpay.js
+++ ConfigData/razorpay.js
@@ -1,8 +1,8 @@
 const Razorpay = require("razorpay");
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: "rzp_live_R9XuwBFRtEokgL",
+  key_secret: "iyF1RlwcQk33labEuBeTkTKI",
 });
 
 module.exports = razorpay;
